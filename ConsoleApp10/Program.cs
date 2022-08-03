476e6d4ec206bfa589d239cc7beba3c63e1beb62
Octa <octavio.macieira@aarim.com>
2022-08-03 20:46:57 -0300

    first commit

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, area, pi;
+
+            pi = 3.14159;
+
+            r = double.Parse(Console.ReadLine());
+
+            area = pi * (4.0 / 3) * Math.Pow(r, 3.0);
+
+            Console.WriteLine("VOLUME = " + area.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
